c4208c0ebce259ace0a213fee3cadcc7569e2e5e
riliu <ilyukroma228@gmail.com>
2026-06-01 14:50:30 +0300

    fix: card component tag

--- src/components/PetProjects/PetProjectsCard.jsx
+++ src/components/PetProjects/PetProjectsCard.jsx
@@ -1,12 +1,15 @@
-/* eslint-disable no-unused-vars */
 function PetProjectsCard({ title, description, url }) {
   return (
-    <section className="bg-slate-800 p-6 flex flex-col gap-4 border-2 border-indigo-500 rounded-xl h-full min-h-[320px]">
+    <section className="bg-slate-800 p-6 flex flex-col gap-4 border-2 border-indigo-500 rounded-xl h-full min-h-[330px]">
       <h1 className="text-white text-xl font-semibold">{title}</h1>
       <p className="text-gray-300 text-base flex-1">{description}</p>
-      <button className="mt-auto text-indigo-500 hover:underline">
+      <a
+        href={url}
+        target="_blank"
+        className="mt-auto text-m text-indigo-500 hover:underline"
+      >
         Learn more
-      </button>
+      </a>
     </section>
   );
 }
