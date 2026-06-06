fcaa2b32d6017225eae3261f8f0fca075ce2e28f
riliu <ilyukroma228@gmail.com>
2026-06-06 21:27:33 +0300

    feat: add logic to MealActions

--- src/components/MealPlanner/MealActions.jsx
+++ src/components/MealPlanner/MealActions.jsx
@@ -1,13 +1,37 @@
 /* eslint-disable no-unused-vars */
 function MealActions({ meal, setMeal, setHistory }) {
+  function handleSave() {
+    setHistory((cur) => [
+      ...cur,
+      {
+        ...meal,
+        date: new Date().toLocaleString(),
+      },
+    ]);
+  }
+
+  function handleReset() {
+    setMeal({
+      main: "",
+      garnish: "",
+      salad: "",
+    });
+  }
+
   const buttonStyle =
     "text-xl text-white border-2 border-indigo-500 rounded-lg p-3 hover:bg-slate-800";
 
   return (
     <div className="flex gap-4 justify-center mt-6">
-      <button className={buttonStyle}>Save meal</button>
+      <button className={buttonStyle}>Random meal</button>
 
-      <button className={buttonStyle}>Reset</button>
+      <button onClick={handleSave} className={buttonStyle}>
+        Save meal
+      </button>
+
+      <button onClick={handleReset} className={buttonStyle}>
+        Reset
+      </button>
     </div>
   );
 }
